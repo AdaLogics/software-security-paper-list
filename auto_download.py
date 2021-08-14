885aaa63337155bc03472f126e885d015b028d17
Cerises <47936572+Ceris3s@users.noreply.github.com>
2021-08-14 17:31:13 +0800

    Add files via upload

--- auto_download.py
+++ auto_download.py
@@ -5,6 +5,9 @@ from pathlib import Path
 
 def download_list(listname):
 
+
+    namecount = 0
+    failcount = 0
     out_dir = os.path.join("out", listname)
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
@@ -14,6 +17,9 @@ def download_list(listname):
     # Get the markdown
     list_readme = "README.md"
 
+    failname = []
+    namelist = []
+
     if listname == "All":
         grab_all = True
         should_grab = True
@@ -22,7 +28,7 @@ def download_list(listname):
         should_grab = False
 
     # Open the list and go through each bullet
-    with open(list_readme, "r") as lrf:
+    with open(list_readme, "r", encoding='UTF-8') as lrf:
 
         for line in lrf:
             # If we have defined a listname then we should only get the papers from this list.
@@ -48,8 +54,10 @@ def download_list(listname):
                     try:
                         start = line.find("[")
                         end = line.find("]")
-                        name = line[start+1:end].replace(" ","_").replace(",", "_")
+                        name = line[start+1:end].replace(" ","_").replace(",", "_").replace(':', '_')
                         print("Name: %s"%(name))
+                        namelist.append(name)
+                        namecount +=1
 
                         # Now get the URL
                         start = line.find("(")
@@ -61,9 +69,40 @@ def download_list(listname):
                         filetowrite=Path(os.path.join(out_dir, "%s.pdf"%(name)))
                         filetowrite.write_bytes(response.content)
                     except:
+                        failname.append(name)
+                        failcount +=1
                         continue
             else:
-                print("Skipping %s"%(line))
+                #print("Skipping %s"%(line))
+                pass
+        
+        
+
+        p = Path(out_dir)
+        fail_flag = False
+        sucess_pdfs = sorted(p.glob('*.pdf'))
+        sucess_name = [pdf.stem for pdf in sucess_pdfs ]
+        for child in p.iterdir():
+            if child in sucess_pdfs:
+                continue
+            else:
+                fail_flag = True
+                failcount +=1
+                child.unlink()
+
+        if fail_flag:
+            print('\n\nThe following files failed to download, please download manually\n')
+            for name in namelist:
+                if name in sucess_name:
+                    continue
+                else:
+                    print(name)
+
+        print("Total papers count :", namecount)
+        print("Failure count :", failcount)
+        print('Sucess count :', namecount - failcount)
+        print('\n\n')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
